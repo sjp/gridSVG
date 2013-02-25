180dbcbdd114b89e4fae428e5d130afb5aa3c59d
Simon Potter <simon@sjp.co.nz>
2013-02-26 11:47:17 +1300

    Make default commentGrob names more obvious.

--- pkg/R/comment.R
+++ pkg/R/comment.R
@@ -4,6 +4,9 @@ grid.comment <- function(comment, name = NULL, vp = NULL) {
 
 commentGrob <- function(comment, name = NULL, vp = NULL) {
     ng <- nullGrob(name = name, vp = vp)
+    # Fix name to be a commentGrob name instead of nullGrob
+    if (is.null(name))
+        ng$name <- gsub("null", "comment", ng$name)
     ng$comment <- comment
     cl <- class(ng)
     class(ng) <- unique(c("comment.grob", cl))
