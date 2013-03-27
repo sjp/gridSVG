a9d6d82c121230603940a12923c0788dace9effa
Simon Potter <simon@sjp.co.nz>
2013-03-27 15:36:34 +1300

    Remove dead code.

--- pkg/R/gridsvg.R
+++ pkg/R/gridsvg.R
@@ -249,20 +249,3 @@ dev.off <- function() {
         grDevices::dev.off()
     }
 }
-
-old.gridToSVG <- function(name="Rplots.svg") {
-  svgdev <- openSVGDev(name, width=par("din")[1], height=par("din")[2])
-  # Start a new page because we are going to be reproducing the
-  # pushing and popping of viewports and this needs to be done
-  # from scratch
-  grid.newpage(recording=FALSE)
-  # Traverse the grid display list producing
-  # SVG equivalents of all grid output
-  # This nastily peeks into the grid NAMESPACE to get the
-  # display list (for now)
-  lapply(grid:::grid.Call("L_getDisplayList"), gridToDev, svgdev)
-  # Before closing, need to pop the top-level viewport
-  # which is not possible in grid
-  devEndGroup(svgdev)
-  devClose(svgdev)
-}
